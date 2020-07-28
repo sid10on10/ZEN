8a8d9c61356c761994413ac851a0d947db83cb25
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-07-28 13:15:49 +0530

    week2 day1 auth

---
+++ Week2/day1/auth/auth.js
@@ -0,0 +1,24 @@
+
+
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var request = new XMLHttpRequest()
+let city = 'varanasi'
+let api_key = '4b83b512b8a0c3028cf6fa4cd0a9f4c2'
+let proxy = 'https://cors-anywhere.herokuapp.com/'
+
+// Open a new connection, using the GET request on the URL endpoint
+let url_string = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+request.open('GET', proxy+url_string, true)
+//request.setRequestHeader('Access-Control-Allow-Origin', 'www.google.com');
+request.send();
+request.onload = function() {
+  // Begin accessing JSON data here
+var data = JSON.parse(this.response)
+console.log(data);
+}
+
+/*
+fetch(url_string)
+.then(res=>res.json())
+.then(data=>console.log(data))
+*/
