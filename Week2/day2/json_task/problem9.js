c688430a3999373d7bbaf47b2543a19c8ad0f0b6
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-07-28 21:53:48 +0530

    problem9 json

---
+++ Week2/day2/json_task/problem9.js
@@ -0,0 +1,19 @@
+var students = [
+    {name: "Siddharth Abhimanyu", age: 21}, { name: "Malar", age: 25},
+    {name: "Maari",age: 18},{name: "Bhallala Deva",age: 17},
+    {name: "Baahubali",age: 16},{name: "AAK chandran",age: 23},   {name:"Gabbar Singh",age: 33},{name: "Mogambo",age: 53},
+    {name: "Munnabhai",age: 40},{name: "Sher Khan",age: 20},
+    {name: "Chulbul Pandey",age: 19},{name: "Anthony",age: 28},
+    {name: "Devdas",age: 56} 
+    ];
+   function returnMinors(arr)
+   {
+       let outarr = []
+    for(i of students){
+        if(i["age"]<20){
+            outarr.push(i["name"])
+        }
+    }
+    return outarr
+   }
+   console.log(returnMinors(students));
