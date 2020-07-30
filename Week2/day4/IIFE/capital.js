3890c6132f09a1f9ba4edd6b65f2a14da8b4de31
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-07-30 19:56:45 +0530

    week2 day4

---
+++ Week2/day4/IIFE/capital.js
@@ -0,0 +1,9 @@
+let arr = ["week1","week2","anony","HiWorld"];
+
+(function(){
+ let newarr = arr.map((data)=>{
+     data = data[0].toUpperCase()+data.slice(1)
+     return data
+    })
+console.log(newarr)
+})();
--- Week2/day4/IIFE/odd.js
+++ Week2/day4/IIFE/odd.js
@@ -1,6 +1,6 @@
 let arr = [1,2,3,4,90,55,100,23,13,12,25]
 
-function(arr){
+(function(){
     let odd_arr = arr.filter(data=>data%2===1)
     console.log("Odd Numbers:",odd_arr)
-}
+})();
