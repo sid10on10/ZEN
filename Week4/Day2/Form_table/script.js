e274025626f6cbb54ed52d4faeee46977ae16655
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-08-12 00:08:46 +0530

    fixed table problem added script.js

--- Week4/Day2/Form_table/script.js
+++ Week4/Day2/Form_table/script.js
@@ -1,19 +1,63 @@
+function getSelected(id){
+    let elem = document.getElementById(id).selectedOptions;
+    let selected = [];
+    for(let i = 0; i < elem.length; i++){
+        selected.push(elem[i].innerText)
+    }
+    return selected;
+}
+
+
+
 function dataInput(){
    let fname =  document.getElementById('firstname').value
    let lname = document.getElementById('lastname').value
    let address = document.getElementById('address').value
    let pincode = document.getElementById('pincode').value
-   let gender = document.getElementById('gender').selectedOptions.value
-   let food1 = document.getElementById('foodchoice').selectedOptions[0].value
-   let food2 = document.getElementById('foodchoice').selectedOptions[1].value
+   let gender = getSelected('gender')
+   let food = getSelected('foodchoice')
+
    let state = document.getElementById('state').value
    let country = document.getElementById('country').value
 
-   
-   let output = document.getElementById('output')
-   output.innerHTML += `<tr><td>${fname}</td><td>${lname}</td><td>${address}</td><td>${pincode}</td><td>${gender}</td><td>${food1}</td><td>${food2}</td><td>${state}</td><td>${country}</td></tr>`
-    setTimeout(dataInput,1000)
+   let data = [{'First Name': fname}, {'Last Name': lname},{'Address': address},{'PinCode': pincode}, {'Food':food}, {'Gender': gender},{'State': state},{'Country': country}];
+   localStorage.setItem('data', JSON.stringify(data))
 }
-dataInput()
 
+function addTableData(){
+    let row = document.createElement('tr')
+    let table = document.getElementById("output")
+    let data = JSON.parse(localStorage.getItem('data'));
 
+    let firstname = document.createElement('td')
+    firstname.innerText = `${data[0]['First Name']}`
+    row.appendChild(firstname)
+    let lastname = document.createElement('td')
+    lastname.innerText = `${data[1]['Last Name']}`
+    row.appendChild(lastname)
+    let address_value = document.createElement('td')
+    address_value.innerText = `${data[2]['Address']}`
+    row.appendChild(address_value)
+    let pincode_value = document.createElement('td')
+    pincode_value.innerText = `${data[3]['PinCode']}`
+    row.appendChild(pincode_value)
+    let gender_value = document.createElement('td')
+    gender_value.innerText = `${data[5]['Gender'].toString()}`
+    row.appendChild(gender_value)
+    let food_value = document.createElement('td')
+    food_value.innerText = `${data[4]['Food'][0].toString()}`
+    row.appendChild(food_value)
+    let food_value1 = document.createElement('td')
+    food_value1.innerText = `${data[4]['Food'][1].toString()}`
+    row.appendChild(food_value1)
+    let state_value = document.createElement('td')
+    state_value.innerText = `${data[6]['State']}`
+    row.appendChild(state_value)
+    let country_value = document.createElement('td')
+    country_value.innerText = `${data[7]['Country']}`
+    row.appendChild(country_value)
+
+    table.appendChild(row)
+    localStorage.setItem('data', null)
+}
+addTableData();
