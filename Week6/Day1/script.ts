c0ef5971e485b25c76d9e523d54a99af0ee55a3a
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-08-25 19:20:59 +0530

    week6 script reduce

--- Week6/Day1/script.ts
+++ Week6/Day1/script.ts
@@ -31,3 +31,13 @@ let chunk  = function (arr:[],k:number){
       }
     }
   }
+
+  // reducer takes an array, reducer() and initialValue as argument
+function reduce(arr, reducer, initialValue) 
+{    
+  let accumulator = initialValue === undefined ? 0 : initialValue        // loop though array    
+  for(let i=0;i<arr.length;i++){      
+   accumulator = reducer(accumulator, arr[i], i, arr);    
+  }
+  return accumulator;
+}
