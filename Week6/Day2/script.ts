9e4c9a63f774e1606f650635e54eb363f572f4fe
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-08-26 18:21:08 +0530

    week6 day2

---
+++ Week6/Day2/script.ts
@@ -0,0 +1,20 @@
+class Pet {
+  name: string;
+  type: string;
+  color: string;
+  age: number;
+  breed: string;
+  history: string;
+  Location: string;
+
+  constructor(name,type,color,age,breed,history,Location) { 
+      this.name = name;
+      this.type = type;
+      this.color = color;
+      this.age = age;
+      this.breed = breed;
+      this.history = history;
+      this.Location = Location;
+  }
+}
+
