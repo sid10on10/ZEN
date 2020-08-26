85a5060ac2377f60a40de1d8d85ccd3211cd2c16
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-08-26 19:35:44 +0530

    week6 day2 request class

--- Week6/Day2/script.ts
+++ Week6/Day2/script.ts
@@ -18,3 +18,42 @@ class Pet {
   }
 }
 
+class request {
+  query: object[][] = [];
+  pets:pet[] =[];
+
+  constructor(){ }
+
+  storeEnquiry(enquery : object[]) { 
+      this.query.push(enquery);
+  }
+
+  enqueryStatus(){
+      let result = []
+      for(let i = 0; i < 5 && i < this.query.length; i++){
+          let search = this.petAvailable(this.query[i]);
+          result.push(search);
+      }
+      return result;
+  }
+
+  petAvailable(queryArr : object[]) { 
+      let res = {pets:[], count:0};
+
+      for(let i in queryArr){
+          let query = queryArr[i];
+          this.pets.forEach((pet)=>{
+              let isAvailable = Object.keys(query).every(
+                  (constrains) => query[constrains] == pet[constrains]  || query[constrains] == '');
+              if(isAvailable){
+                  let petData = {name : pet.name}
+                  res.pets.push(petData);
+                  res.count++;
+              }
+          });
+      }
+      return res
+  }
+
+}
+
