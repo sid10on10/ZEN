6a7f8944765d45ce35cf3a6f7439bfb95989f1e3
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-08-27 22:25:35 +0530

    transcompile

---
+++ Week6/Day4/script.ts
@@ -0,0 +1,41 @@
+class Card{
+    url:string;
+
+    constructor(url:string){
+        this.url = url;
+        document.getElementById("audio-holder").innerHTML += `<div class="row"><audio controls><source src="${this.url}" type="audio/mp4a"></audio></div>`
+    }
+}
+
+(<HTMLButtonElement>document.getElementById("top")).addEventListener("click",
+async (e)=>{
+    let response = await fetch("https://shazam.p.rapidapi.com/songs/list-artist-top-tracks?locale=en-US&id=40008598",
+        {
+          method: "GET",
+          headers: {
+            "x-rapidapi-host": "shazam.p.rapidapi.com",
+            "x-rapidapi-key": "eab956df1cmshfa8f3a61aed24f8p19dd6cjsn66351b737de1",
+          },
+        }
+      )
+    let data = await response.json();
+    console.log(data)
+}
+);
+
+(<HTMLButtonElement>document.getElementById("recommend")).addEventListener("click",
+async (e)=>{
+    let response = await fetch("https://shazam.p.rapidapi.com/songs/list-recommendations?locale=en-US&key=484129036", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "shazam.p.rapidapi.com",
+            "x-rapidapi-key": "eab956df1cmshfa8f3a61aed24f8p19dd6cjsn66351b737de1"
+        }
+    })
+    let data = await response.json();
+    console.log(data)
+}
+);
+
+
+
