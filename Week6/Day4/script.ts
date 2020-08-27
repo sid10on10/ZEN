e339c4d0b8b7edbe0127e78c4b71fd504dfe1947
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-08-27 23:53:45 +0530

    transcompile error

--- Week6/Day4/script.ts
+++ Week6/Day4/script.ts
@@ -3,10 +3,16 @@ class Card{
 
     constructor(url:string){
         this.url = url;
-        document.getElementById("audio-holder").innerHTML += `<div class="row"><audio controls><source src="${this.url}" type="audio/mp4a"></audio></div>`
+        (<HTMLDivElement>document.getElementById("audio-holder")).innerHTML += `<div class="row"><audio controls><source src="${this.url}" type="audio/mp4a"></audio></div>`
     }
 }
-
+interface RapidAPI{
+    hub:{
+        actions:[{
+            uri:string;
+        }]
+    }
+}
 (<HTMLButtonElement>document.getElementById("top")).addEventListener("click",
 async (e)=>{
     let response = await fetch("https://shazam.p.rapidapi.com/songs/list-artist-top-tracks?locale=en-US&id=40008598",
@@ -19,7 +25,9 @@ async (e)=>{
         }
       )
     let data = await response.json();
-    console.log(data)
+    data.tracks.forEach((element:RapidAPI) => {
+        console.log(element.hub.actions[1].uri)
+    });
 }
 );
 
@@ -31,9 +39,9 @@ async (e)=>{
             "x-rapidapi-host": "shazam.p.rapidapi.com",
             "x-rapidapi-key": "eab956df1cmshfa8f3a61aed24f8p19dd6cjsn66351b737de1"
         }
-    })
+    });
     let data = await response.json();
-    console.log(data)
+    console.log(data);
 }
 );
 
