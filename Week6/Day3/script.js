cb9c39fc45c7cabeaff3f813411408516d79b004
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-08-29 22:39:36 +0530

    TV some area not working

--- Week6/Day3/script.js
+++ Week6/Day3/script.js
@@ -1,33 +1,120 @@
-var Tv = /** @class */ (function () {
-    function Tv(parameters) {
-        this.size = parameters.size;
-        this.type = parameters.type;
-        this.color = parameters.color;
-        this.brand = parameters.brand;
-        this.price = parameters.price;
-        this.channel = parameters.channel;
-        this.volume = parameters.volume;
+var videos = [
+    {
+        "sources": ["http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4"],
+        "title": "For Bigger Escape"
+    },
+    {
+        "sources": ["http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4"],
+        "title": "For Bigger Fun"
+    },
+    {
+        "sources": ["http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4"],
+        "title": "For Bigger Joyrides"
+    },
+    {
+        "sources": ["http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4"],
+        "title": "For Bigger Meltdowns"
+    },
+    {
+        "sources": ["http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4"],
+        "title": "Sintel"
+    },
+    {
+        "sources": ["http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4"],
+        "title": "Subaru Outback On Street And Dirt"
+    },
+    {
+        "sources": ["http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"],
+        "title": "Tears of Steel"
+    },
+    {
+        "sources": ["http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4"],
+        "title": "Volkswagen GTI Review"
+    },
+    {
+        "sources": ["http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4"],
+        "title": "We Are Going On Bullrun"
+    },
+    {
+        "sources": ["http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4"],
+        "title": "What care can you get for a grand?"
     }
-    Tv.prototype.increaseVolume = function () {
-        if (this.volume < 100) {
-            this.volume++;
+];
+var TV = /** @class */ (function () {
+    function TV(price, inches, OnOFF, brand, channel, volume) {
+        if (OnOFF === void 0) { OnOFF = false; }
+        if (brand === void 0) { brand = "MI"; }
+        if (channel === void 0) { channel = 1; }
+        if (volume === void 0) { volume = 25; }
+        this.price = price;
+        this.inches = inches;
+        this.OnOFF = OnOFF;
+        this.brand = brand;
+        this.channel = channel;
+        this.volume = volume;
+        this.player = document.getElementById('video_player');
+        this.notification = document.getElementById('notification');
+    }
+    TV.prototype.setVolume = function (volume) {
+        if (this.OnOFF && volume > 0 && volume < 100) {
+            this.volume = volume;
+            this.notification.innerHTML = "Volume<br>" + this.volume;
+            setTimeout(this.fadeNot, 2000);
         }
     };
-    Tv.prototype.decreaseVolume = function () {
-        if (this.volume > 0) {
-            this.volume--;
+    TV.prototype.setChannel = function (channel) {
+        if (this.OnOFF && channel > 0 && channel < 10) {
+            this.channel = channel;
+            this.player.src = videos[this.channel - 1].sources;
+            this.notification.innerHTML = "Channel " + this.channel;
         }
     };
-    Tv.prototype.setChannel = function (value) {
-        if (value > 50 && value < 100) {
-            this.channel = value;
+    TV.prototype.mute = function () {
+        if (this.OnOFF) {
+            this.player.muted = !this.player.muted;
         }
     };
-    Tv.prototype.increaseChannel = function () {
-        this.channel++;
+    TV.prototype.playPause = function () {
+        if (this.OnOFF) {
+            if (this.player.paused) {
+                this.player.play();
+            }
+            else {
+                this.player.pause();
+            }
+        }
     };
-    Tv.prototype.decreaseChannel = function () {
-        this.channel--;
+    TV.prototype.OnOffbtn = function () {
+        this.OnOFF = !this.OnOFF;
+        if (this.OnOFF) {
+            this.player.src = videos[this.channel - 1].sources;
+        }
+        else {
+            this.player.src = "";
+        }
     };
-    return Tv;
+    TV.prototype.fadeNot = function () {
+        this.notification.innerHTML = "";
+    };
+    return TV;
 }());
+var myTv = new TV(12000, 40);
+console.log(myTv.volume);
+document.getElementById("+").addEventListener("click", function () {
+    console.log("+");
+});
+document.getElementById("-").addEventListener("click", function () {
+    console.log("-");
+});
+document.getElementById("next").addEventListener("click", function () {
+    console.log("next");
+});
+document.getElementById("prev").addEventListener("click", function () {
+    console.log("prev");
+});
+document.getElementById("power").addEventListener("click", function () {
+    console.log("power");
+});
+document.getElementById("mute").addEventListener("click", function () {
+    console.log("mute");
+});
--- Week6/Day3/script.ts
+++ Week6/Day3/script.ts
@@ -1,50 +1,147 @@
-interface Size{
-    width:number;
-    height:number;
-}
 
-class Tv {
-    size:Size;
-    type:string;
-    color:string;
-    brand:string;
+let videos = [ 
+    {
+      "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4" ],
+      "title" : "For Bigger Escape"
+    },
+    {
+      "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4" ],
+      "title" : "For Bigger Fun"
+    },
+    {
+      "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4" ],
+      "title" : "For Bigger Joyrides"
+    },
+    {
+      "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4" ],
+      "title" : "For Bigger Meltdowns"
+    },
+    {
+      "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4" ],
+      "title" : "Sintel"
+    },
+    {
+      "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4" ],
+      "title" : "Subaru Outback On Street And Dirt"
+    },
+    {
+      "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4" ],
+      "title" : "Tears of Steel"
+    },
+    {
+      "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4" ],
+      "title" : "Volkswagen GTI Review"
+    },
+    {
+      "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4" ],
+      "title" : "We Are Going On Bullrun"
+    },
+    {
+      "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4" ],
+      "title" : "What care can you get for a grand?"
+    }
+]
+
+class TV {
     price:number;
+    inches:number;
+    OnOFF:boolean;
+    brand:string;
     channel:number;
     volume:number;
-    constructor(parameters){
-        this.size = parameters.size;
-        this.type = parameters.type;
-        this.color = parameters.color;
-        this.brand = parameters.brand;
-        this.price = parameters.price;
-        this.channel = parameters.channel;
-        this.volume = parameters.volume;
+    player;
+    notification;
+
+    constructor(price, inches, OnOFF = false, brand="MI", channel=1, volume=25){
+		this.price = price
+		this.inches = inches
+		this.OnOFF = OnOFF
+		this.brand = brand
+		this.channel = channel
+        this.volume = volume
+        this.player = document.getElementById('video_player');
+        this.notification = document.getElementById('notification');
     }
 
-    increaseVolume(){
-        if(this.volume<100){
-            this.volume++;
+    setVolume(volume){
+        if(this.OnOFF && volume>0 && volume<100){
+            this.volume = volume
+            this.notification.innerHTML = `Volume<br>${this.volume}`
+            setTimeout(this.fadeNot,2000)
         }
     }
 
-    decreaseVolume(){
-        if(this.volume>0){
-            this.volume--;
+    setChannel(channel){
+        if(this.OnOFF && channel>0 && channel<10){
+            this.channel = channel;
+            this.player.src = videos[this.channel-1].sources;
+            this.notification.innerHTML = `Channel ${this.channel}`;
         }
     }
 
-    setChannel(value){
-        if(value>50 && value <100){
-            this.channel = value;
+    mute(){
+        if(this.OnOFF){
+            this.player.muted = !this.player.muted
         }
     }
 
-    increaseChannel(){
-        this.channel++;
+    playPause(){
+        if(this.OnOFF){
+            if(this.player.paused){
+                this.player.play();
+            }else{
+                this.player.pause();
+            }
+        }
     }
 
-    decreaseChannel(){
-        this.channel--;
+    OnOffbtn(){
+        this.OnOFF = !this.OnOFF
+        if(this.OnOFF){
+            this.player.src = videos[this.channel-1].sources
+        }else{
+            this.player.src = "";
+        }
+    }
+
+    fadeNot(){
+        this.notification.innerHTML = "";
     }
 
 }
+
+let myTv = new TV(12000,40)
+console.log(myTv.volume)
+
+document.getElementById("+").addEventListener("click",function(){
+    console.log("+")
+    myTv.setVolume(myTv.volume+1);
+})
+
+document.getElementById("-").addEventListener("click",function(){
+    console.log("-")
+    myTv.setVolume(myTv.volume-1);
+})
+
+document.getElementById("next").addEventListener("click",function(){
+    console.log("next")
+    myTv.setChannel(myTv.channel+1);  
+})
+
+document.getElementById("prev").addEventListener("click",function(){
+    console.log("prev")
+    myTv.setChannel(myTv.channel-1);  
+})
+
+document.getElementById("power").addEventListener("click",function(){
+    console.log("power")
+    myTv.OnOffbtn()
+})
+
+document.getElementById("mute").addEventListener("click",function(){
+    console.log("mute")
+    myTv.mute()
+})
+
+
+
