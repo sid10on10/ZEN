b04be85e5fe879a3e8811ff8999e00c14f12171b
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-09-24 21:57:25 +0530

    day2 task

---
+++ Week10/Day2-react/task/build/precache-manifest.e8b45e769c50da61d606e46d7cd49f45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a8054a4ffc6f1f96df41e9be24b46a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64fa870b670ce98fcab6",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "631e961ecc420674d2cf",
+    "url": "/static/js/2.d9093ec4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d9093ec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64fa870b670ce98fcab6",
+    "url": "/static/js/main.798d7aaa.chunk.js"
+  },
+  {
+    "revision": "a09792300aedfe1060d4",
+    "url": "/static/js/runtime-main.d8921639.js"
+  }
+]);
